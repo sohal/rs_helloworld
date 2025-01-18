ba573e2e7535436a51addb857265f6b319244055
sohal <tosohal@gmail.com>
2025-01-18 21:08:38 +0100

    Add tools
    This commit:
    * adds the ruby version (rbenv)
    * adds the python version (pyenv)
    * adds pre-commit configuration

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-use std::str::FromStr;
 use std::env;
+use std::mem;
+use std::str::FromStr;
 fn main() {
     let mut number: Vec<u64> = Vec::new();
 
     for arg in env::args().skip(1) {
         number.push(u64::from_str(&arg).expect("Error parsing arguement"));
     }
-    if number.len() == 0 {
+    if number.is_empty() {
         eprintln!("Usage: gcd NUMBER ...");
         std::process::exit(1);
     }
@@ -19,15 +20,17 @@ fn main() {
     println!("The greatest common divisor of {:?} is {}", number, d);
 }
 
-fn gcd (mut n: u64, mut m: u64) -> u64 {
+/// function
+/// `gcd`
+fn gcd(mut n: u64, mut m: u64) -> u64 {
     assert!(m != 0 && n != 0);
     while m != 0 {
         if m < n {
             let t = m;
-            m = n;
+            mem::swap(&mut m, &mut n);
             n = t;
         }
-        m = m % n;
+        m %= n;
     }
     n
 }
@@ -36,10 +39,5 @@ fn gcd (mut n: u64, mut m: u64) -> u64 {
 fn test_gcd() {
     assert_eq!(gcd(14, 15), 1);
 
-    assert_eq!(gcd(
-        2 * 3 * 5 * 11,
-        3 * 5 * 7 * 11 * 19),
-        165     );
+    assert_eq!(gcd(2 * 3 * 5 * 11, 3 * 5 * 7 * 11 * 19), 165);
 }
-
-
